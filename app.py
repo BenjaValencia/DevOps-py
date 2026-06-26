301bda50d168dae21693c5a2a43cf7947e578c17
BenjaValencia <valenciabenjamin046@gmail.com>
2026-06-26 03:47:04 -0400

    Update app.py
    se cambia el port desde app.py, porque no toma bien en las tareas de ecs

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ CORS(app)
 # Database configuration
 db_config = {
     'host': os.getenv('DB_HOST', 'localhost'),
-    'port': int(os.getenv('DB_PORT', 3306)),
+    'port': 3306,
     'user': os.getenv('DB_USER', 'root'),
     'password': os.getenv('DB_PASSWORD', ''),
     'database': os.getenv('DB_NAME', 'products_db')
